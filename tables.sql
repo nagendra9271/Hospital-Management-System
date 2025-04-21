123186a1e1a1e441e36c688f471b0f8b4f924396
nagendra9271 <nagendrakandula2006@gmail.com>
2025-04-21 11:14:11 +0530

    Merge remote-tracking branch 'origin/main' into main

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 use hms;
 
 CREATE TABLE users(
-    u_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
